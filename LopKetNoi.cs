7220ae0e9089820f2fd8d7a88c6736d13a00b69a
leconghoang <155082170+leconghoang@users.noreply.github.com>
2024-06-03 22:24:04 +0700

    Add files via upload

---
+++ LopKetNoi.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+namespace ktracuoiki
+
+{
+    public class LopKetNoi
+    {
+        SqlConnection con;
+        private void MoKetNoi()
+        {
+            string sqlcon = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\C++\\ktracuoiki\\ktracuoiki\\App_Data\\Database1.mdf;Integrated Security=True";
+            con = new SqlConnection(sqlcon);
+            con.Open();
+
+        }
+        private void DongKetNoi()
+        {
+            if (con.State == ConnectionState.Open)
+                con.Close();
+        }
+        public DataTable DocDuLieu(string sql)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                MoKetNoi();
+                SqlDataAdapter da = new SqlDataAdapter(sql, con);
+                da.Fill(dt);
+            }
+            catch
+            {
+                dt = null;
+            }
+            finally
+            {
+                DongKetNoi();
+            }
+            return dt;
+        }
+       
+    }
+}
---
+++ MasterPage.Master.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ktracuoiki
+{
+    public partial class MasterPage : System.Web.UI.MasterPage
+    {
+        LopKetNoi ketNoi=new LopKetNoi();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+            {
+                return;
+
+            }
+            string sql = "select * from LOAITHIETBI ";
+            DLLOAI.DataSource = ketNoi.DocDuLieu(sql);
+            DLLOAI.DataBind(); 
+        }
+    }
+}
